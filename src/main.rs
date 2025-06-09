f0abfae02d859cf108a4a223a5661c8847f234e5
Abdul Amaan <amaan.programmer1@gmail.com>
2025-06-09 22:13:34 +0530

    learned more about error handling

--- src/main.rs
+++ src/main.rs
@@ -1,184 +1,67 @@
-use rand;
-use std::{
-    collections::HashMap,
-    io::{self, Write},
-};
-enum SpreadsheetCell {
-    Int(i32),
-    Float(f64),
-    Text(String),
+use std::fs;
+use std::fs::File;
+use std::io::ErrorKind;
+use std::io::{self,Read};
+use std::error::Error;
+
+// fn read_username_from_file() -> Result<String,io::Error> {
+//     let username_file_result = File::open("hello.txt");
+    
+//     let mut username_file = match username_file_result {
+//         Ok(file) => file,
+//         Err(e) => return Err(e),
+//     };
+    
+//     let mut username = String::new();
+    
+//     match username_file.read_to_string(&mut username) {
+//         Ok(_) => Ok(username),
+//         Err(e) => Err(e)
+//     }
+// }
+
+fn read_username_from_file_normal_version() -> Result<String,io::Error> {
+    let mut username_file = File::open("hello.txt")?;
+    let mut username = String::new();
+    username_file.read_to_string(&mut username)?;
+    Ok(username)
 }
 
-fn main() {
-    let mut v: Vec<i32> = Vec::new();
-    let v2 = vec![1, 2, 3];
+fn read_username_from_file_shorter_verison() -> Result<String,io::Error> {
 
-    v.push(5);
-    v.push(6);
-    v.push(7);
-    v.push(8);
+    let mut username = String::new();
+    File::open("hello.txt")?.read_to_string(&mut username)?;
+    Ok(username)
+}
 
-    let v3 = vec![1, 2, 3, 4, 5];
+fn read_username_from_file_the_shortest_version() -> Result<String,io::Error> {
+    fs::read_to_string("hello.txt")
+}
 
-    let third: &i32 = &v[2];
-    println!("The third element is {third}");
+fn last_char_of_first_line(text: &str) -> Option<char> {
+    text.lines().next()?.chars().last()
+}
 
-    let third: Option<&i32> = v.get(2);
-
-    match third {
-        Some(third) => println!("the third element is {third}"),
-        None => println!("These is no third element"),
-    }
-
-    let mut v4 = vec![100, 32, 57];
-    for i in &v4 {
-        println!("{i}")
-    }
-
-    for i in &mut v4 {
-        *i += 50;
-    }
-
-    for i in &v4 {
-        println!("{i}")
-    }
-
-    let row = vec![
-        SpreadsheetCell::Int(3),
-        SpreadsheetCell::Text(String::from("blue")),
-        SpreadsheetCell::Float(10.12),
-    ];
-
-    let mut s = String::new();
-
-    let data = "initial contents";
-
-    let s1 = data.to_string();
-    let s2 = "initial contents".to_string();
-
-    s.push_str("bar");
-
-    let s1 = String::from("hello, ");
-    let s2 = String::from("world!");
-    let res = s1 + &s2;
-
-    let s1 = String::from("tic");
-    let s2 = String::from("tac");
-    let s3 = String::from("toe");
-
-    let s = format!("{s1}-{s2}-{s3}");
-    println!("{s}");
-
-    let hello = "नमस्ते";
-
-    for c in hello.chars() {
-        println!("{c}");
-    }
-
-    for b in hello.bytes() {
-        println!("{b}");
-    }
-
-    let mut scores = HashMap::new();
-
-    scores.insert(String::from("Blue"), 10);
-    scores.insert(String::from("Yellow"), 50);
-
-    let team_name = String::from("Blue");
-    let score = scores.get(&team_name).copied().unwrap_or(0);
-
-    for (key, value) in scores {
-        println!("{key}: {value}");
-    }
-
-    let mut scores = HashMap::new();
-    scores.insert(String::from("Blue"), 10);
-    scores.insert(String::from("Blue"), 25);
-
-    println!("{scores:?}");
-
-    scores.insert(String::from("Blue"), 10);
-
-    scores.entry(String::from("Yellow")).or_insert(50);
-    scores.entry(String::from("Blue")).or_insert(50);
-
-    println!("{scores:?}");
-
-    let mut vec = Vec::new();
-    let mut hashmap: HashMap<i32, i32> = HashMap::new();
-    for _ in 0..101 {
-        let ele = rand::random_range(0..101);
-        println!("ele: {ele}");
-        vec.push(ele);
-        let count = hashmap.entry(ele).or_insert(0);
-        *count += 1;
-    }
-    let mut mode = -1;
-    let mut max = 0;
-    for (key, value) in hashmap {
-        println!("{key}:{value}");
-        if max < value {
-            max = value;
-            mode = key;
+fn main() -> Result<(), Box<dyn Error>> {
+    let v = vec![1,2,3];
+    
+    //v[99];
+    
+    let greeting_file_result = File::open("hello.txt");
+    
+    let greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {e:?}"),
+            },
+            _ => {
+                panic!("Problem opening the file: {error:?}")
+            }
         }
-    }
-
-    let median = vec[vec.len() / 2];
-    println!("Median: {median}");
-    println!("Mode: {mode}");
-
-    let original = String::from("first");
-    let mut str = original.clone();
-    let mut first_char = 'a';
-    for char in str.chars() {
-        first_char = char;
-        break;
-    }
-
-    if first_char == 'a'
-        || first_char == 'e'
-        || first_char == 'i'
-        || first_char == 'o'
-        || first_char == 'u'
-    {
-        str.push_str("hay");
-    } else {
-        str = String::from(&str[1..str.len()]);
-        str.push(first_char);
-        str.push_str("ay");
-    }
-
-    println!("Original String: {original}");
-    println!("Pig Latin: {str}");
-
-    use std::io;
-
-    let mut data: HashMap<String, Vec<String>> = HashMap::new();
-    let mut response = String::new();
-    let mut count = 0;
-    while count < 5 {
-        io::stdout().flush().unwrap();
-        io::stdin()
-            .read_line(&mut response)
-            .expect("Did not enter correct string");
-        let values = Vec::from_iter(response.split(" "));
-        if values[0] != "Add" || values[2] != "to" {
-            println!("Please Enter in following format: Add <Person> to <Department>");
-        } else {
-            count += 1;
-            let dept = values[3];
-            let name = values[1];
-            println!("{dept}:{name}");
-            let vec = data.entry(String::from(values[3])).or_insert(vec![]);
-            vec.push(String::from(values[1]));
-            response.clear();
-        }
-    }
-    println!("{data:?}");
-    for (key, value) in &data {
-        println!("Department: {key}");
-        for name in value {
-            println!("\t{name}");
-        }
-    }
+    };
+    let greeting_file = File::open("hello.txt")?;
+    
+    Ok(())
 }
